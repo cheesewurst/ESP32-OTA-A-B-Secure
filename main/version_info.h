4b9d51aedbe54a013be3759d1e709e26133895b6
Simon <simonschr@live.nl>
2026-03-16 15:39:20 +0700

    implemented version info

--- main/version_info.h
+++ main/version_info.h
@@ -1,6 +1,8 @@
 #ifndef VERSION_INFO_H
 #define VERSION_INFO_H
 
+#include <stdio.h>
+
 /**
  * @file boot_counter.h
  * @brief Header file for the Version Info component, which tracks the version info of the firmware. 
@@ -9,5 +11,14 @@
  * @date 2026-03-16
  * @version 1.0
  */
+
+typedef struct {
+    const char* version;
+    const char* build_date;
+    const char* git_hash;
+} version_info_t;
+
+void print_version_info(void);
+const version_info_t* get_version_info(void);
 
 #endif
